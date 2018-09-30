9af3edf1f9106107ec3850e85aa282046a3c218f
Matti Tanskanen <mattitapanitanskanen@gmail.com>
2018-09-30 11:23:54 +0300

    Initial frontpage template

---
+++ front-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the front page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#front-page-display
+ *
+ * @package pilot
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+<h1>helloust hello</h1>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
